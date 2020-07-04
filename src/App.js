8693928f1b78165f66f80057048bae82e6417100
Ammar <ammar4568@gmail.com>
2020-07-04 17:48:31 +0500

    5. Adding and Using Radium (Package for using Psuedo Selectors in inline-styling)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -43,7 +44,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -66,6 +71,11 @@ class App extends Component {
       );
 
       btnStyle.backgroundColor = 'red';
+      btnStyle[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+
     }
 
     const classes = [];
@@ -89,4 +99,4 @@ class App extends Component {
 }
 
 
-export default App;
+export default Radium(App); // Radium is used for pseudo selectors
