6b9d295ba2a0c728be7ddb67ece0bbd95e1466b1
Ammar <ammar4568@gmail.com>
2020-07-09 21:50:48 +0500

    12. Cleaning up with Lifecycle Hooks & useEffect()

--- src/components/Cockpit/cockpit.js
+++ src/components/Cockpit/cockpit.js
@@ -7,13 +7,32 @@ const Cockpit = (props) => {
         setTimeout(() => {
             console.log('Get Data from Cloud')
         }, 1000);
+
+        return(() => { // Works as componentWillUnmount
+            console.log('[Cockpit.js] cleanup work in useEffect')
+        })
     }, []); // this will run for first time only
 
     useEffect(() => {
-        console.log('[Cockpit.js] useEffectPersons')
+        console.log('[Cockpit.js] useEffect')
         setTimeout(() => {
             console.log('Get Persons Data from Cloud')
         }, 1000);
+
+        return(() => {
+            console.log('[Cockpit.js] clearkup work in useEffect');
+        })
+    }); // Works every time
+
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect Persons')
+        setTimeout(() => {
+            console.log('Get Persons Data from Cloud')
+        }, 1000);
+
+        return(() => {
+            console.log('[Cockpit.js] clearkup work in persons useEffect');
+        })
     }, [props.persons]); // this will run when persons change
 
     let btnClass = '';
