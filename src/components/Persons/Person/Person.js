a0c08471c1d8378293afec420c182d04210d3a72
Ammar <ammar4568@gmail.com>
2020-07-10 10:18:13 +0500

    24. Passing Unknown Props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,19 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import Auxiliary from '../../hoc/Auxiliary'
+import withClass from '../../hoc/withClass'
 import classes from "./Person.module.css";
 
 const Person = (props) => {
     console.log('[Person.js] render')
     return (
-        <Fragment>
+        <Auxiliary>
             {/* <div className={classes.Person}> */}
             <p onClick={props.click}>I'm a {props.name} aged {props.age}!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
             {/* // </div> */}
-        </Fragment>
+        </Auxiliary>
     )
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
