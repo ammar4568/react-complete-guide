4fdb4e8e8a219262d66b7813ebfad1ac000d40c8
Ammar <ammar4568@gmail.com>
2020-07-10 09:16:28 +0500

    21. Using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import Auxiliary from '../../hoc/Auxiliary'
+import React, { Fragment } from 'react'
 import classes from "./Person.module.css";
 
 const Person = (props) => {
     console.log('[Person.js] render')
     return (
-        <Auxiliary>
-        {/* <div className={classes.Person}> */}
+        <Fragment>
+            {/* <div className={classes.Person}> */}
             <p onClick={props.click}>I'm a {props.name} aged {props.age}!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        {/* // </div> */}
-        </Auxiliary>
+            {/* // </div> */}
+        </Fragment>
     )
 }
 
